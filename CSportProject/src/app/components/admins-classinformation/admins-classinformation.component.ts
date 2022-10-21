79f505ce2d18c86b84120d6cec1471f9b5dc82b6
Jaegyeom Kim <jaegyeom1215@gmail.com>
2022-10-20 17:44:42 -0700

    require push
    require push

---
+++ CSportProject/src/app/components/admins-classinformation/admins-classinformation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admins-classinformation',
+  templateUrl: './admins-classinformation.component.html',
+  styleUrls: ['./admins-classinformation.component.css']
+})
+export class AdminsClassinformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
