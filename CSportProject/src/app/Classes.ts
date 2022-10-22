c0001984cdd08bef7fbba494602ef926e614ed22
Sean <seankel@comcast.net>
2022-10-21 17:55:29 -0700

    Added old class interface to fix errors

--- CSportProject/src/app/Classes.ts
+++ CSportProject/src/app/Classes.ts
@@ -1,9 +1,14 @@
 export interface Class {
-    CID: string;
-    Name: string;
-    Descript: string;
-    STime: string;
-    ETime: string;
-    Date: string;
-    ClassSeats: string; //This should just be a string no array
+    id: number;
+    className: string;
+    classDesc: string;
+    classStart: string;
+    classEnd: string;
+    // CID: string;
+    // Name: string;
+    // Descript: string;
+    // STime: string;
+    // ETime: string;
+    // Date: string;
+    // ClassSeats: string; //This should just be a string no array
 }
