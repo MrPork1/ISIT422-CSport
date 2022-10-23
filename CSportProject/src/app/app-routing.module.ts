74cc4de8d1853e54697fed2b91e1a1bbcc41268c
Jaegyeom Kim <jaegyeom1215@gmail.com>
2022-10-22 18:02:17 -0700

    Add button
    Add button

--- CSportProject/src/app/app-routing.module.ts
+++ CSportProject/src/app/app-routing.module.ts
@@ -7,14 +7,16 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AdminsComponent } from './components/admins/admins.component';
-
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full'},
   { path: 'signin', component: SigninComponent},
   { path: 'signup', component: SignupComponent},
   { path: 'classes', component: ClassViewComponent},
   { path: 'profile', component: ProfileComponent},
-  { path: 'admin', component: AdminsComponent}
+  { path: 'admin', component: AdminsComponent},
+  { path: 'admin-dashboard', component: AdminDashboardComponent}
+
 
 ];
 
