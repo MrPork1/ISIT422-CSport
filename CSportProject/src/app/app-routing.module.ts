596ae945ff03bacee42529299388de11664e3577
Jaegyeom Kim <jaegyeom1215@gmail.com>
2022-10-22 17:43:52 -0700

    Test
    Test

--- CSportProject/src/app/app-routing.module.ts
+++ CSportProject/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { AdminsComponent } from './components/admins/admins.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'signin', pathMatch: 'full'},
@@ -14,8 +15,8 @@ const routes: Routes = [
   { path: 'signup', component: SignupComponent},
   { path: 'classes', component: ClassViewComponent},
   { path: 'profile', component: ProfileComponent},
-  { path: 'admin', component: AdminsComponent}
-
+  { path: 'admin', component: AdminsComponent},
+  { path: 'admin-dashboard', component: AdminDashboardComponent},
 ];
 
 @NgModule({
