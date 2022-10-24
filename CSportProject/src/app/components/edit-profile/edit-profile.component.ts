c010ae9f36feb3c0cf4bd330e3e0ba77d1aaa28f
Sean <seankel@comcast.net>
2022-10-23 21:21:54 -0700

    Added editprofile

--- CSportProject/src/app/components/edit-profile/edit-profile.component.ts
+++ CSportProject/src/app/components/edit-profile/edit-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/User';
 
 @Component({
   selector: 'app-edit-profile',
@@ -11,16 +14,36 @@ export class EditProfileComponent implements OnInit {
   fname !: string;
   lname !: string;
   bday !: string;
-  role !: string;
   password !: string;
+  
+  user !: User;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.user = this.authService.returnUserObject();
+    this.fillValues(this.user);
+    console.log(this.user);
+  }
+
+  fillValues(user: User) {
+    this.email = user.Email;
+    this.fname = user.Fname;
+    this.lname = user.Lname;
+    this.bday = user.Birthday;
   }
 
   applyChanges() {
-    
-  }
+    const newUser = this.user;
 
+    newUser.Fname = this.fname;
+    newUser.Lname = this.lname;
+    newUser.Birthday = this.bday;
+
+    //editUser hasn't been written yet. ill write it in sprint3 - sean
+    //this.userService.editUser(user);
+  }
 }
