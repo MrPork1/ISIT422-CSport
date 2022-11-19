d90b425bc69ae18cc9df4383557eca31a7265fc7
Jaegyeom Kim <jaegyeom1215@gmail.com>
2022-11-19 00:48:41 -0800

    Admin-Class Style, table and function fixed
    Admin-Class Style, table and function fixed

--- CSportProject/src/app/components/admins-classinfo/admins-classinfo.component.ts
+++ CSportProject/src/app/components/admins-classinfo/admins-classinfo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit , Input, Output, EventEmitter} from '@angular/core';
 import { Class } from 'src/app/Classes';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faPenNib } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-admins-classinfo',
   templateUrl: './admins-classinfo.component.html',
@@ -24,10 +25,13 @@ export class AdminsClassinfoComponent implements OnInit {
   end_fix !: string;
   sets_fix!: number;
   price_fix !: number;
+  date_fix !:string;
   // check_add !: boolean;
 
 
   faTimes = faTimes; // 2. From angular-fontawesome
+  faPenNib = faPenNib; // 2. From angular-fontawesome
+
   constructor() { }
 
   ngOnInit(): void {
@@ -47,6 +51,7 @@ export class AdminsClassinfoComponent implements OnInit {
     class2.STime = this.start_fix;
     class2.ETime = this.end_fix;
     class2.Price = this.price_fix;
+    class2.Date = this.date_fix;
     class2.ClassSeats = this.sets_fix.toString();
     this.check_edit = false;
     this.onUpateClass.emit(class2);
@@ -60,6 +65,7 @@ export class AdminsClassinfoComponent implements OnInit {
     this.start_fix = class2.STime;
     this.end_fix = class2.ETime;
     this.price_fix = class2.Price;
+    this.date_fix = class2.Date;
     this.sets_fix = Number(class2.ClassSeats);
 
     this.check_edit = !this.check_edit;
