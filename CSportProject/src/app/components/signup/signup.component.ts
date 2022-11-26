4ad80cc073b94901c7063736531ccbca6b0086b9
Sean <seankel@comcast.net>
2022-11-26 00:07:10 -0800

    Consolidated signin/up css's

--- CSportProject/src/app/components/signup/signup.component.ts
+++ CSportProject/src/app/components/signup/signup.component.ts
@@ -7,7 +7,7 @@ import {User} from '../../User';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css'], 
+  styleUrls: ['../signin/signin.component.css', './signup.component.css'], 
 })
 export class SignupComponent implements OnInit {
 
