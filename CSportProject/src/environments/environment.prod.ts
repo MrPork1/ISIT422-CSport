1e61694fe66d6ec610ecefd85ff57a8e1bce3901
Sean <seankel@comcast.net>
2022-11-28 21:15:33 -0800

    Updated environment.prod.ts to work for build

--- CSportProject/src/environments/environment.prod.ts
+++ CSportProject/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBGZn_LiAXUEtliycKW3HR7fAfebHbR304",
+    authDomain: "fir-c6384.firebaseapp.com",
+    projectId: "fir-c6384",
+    storageBucket: "fir-c6384.appspot.com",
+    messagingSenderId: "477617212489",
+    appId: "1:477617212489:web:3bcfc6794be112f0270398",
+    measurementId: "G-N2TPZ8KNMJ"
+  }
 };
