741eaaa55578e63cf94bf60c02ebe52411c414cb
Jaegyeom Kim <jaegyeom1215@gmail.com>
2022-12-04 18:22:27 -0800

    Annotating code explain
    Annotating code explain

--- CSportProject/src/app/components/admin-avaliable-class-list/admin-avaliable-class-list.component.ts
+++ CSportProject/src/app/components/admin-avaliable-class-list/admin-avaliable-class-list.component.ts
@@ -11,25 +11,35 @@ import { User } from 'src/app/User';
   styleUrls: ['./admin-avaliable-class-list.component.css']
 })
 export class AdminAvaliableClassListComponent implements OnInit {
+
+  // Data binding from admins-usersinfo-unit to here. 
   @Input()
   class !: Class;
+
+  // Data binding from admins-usersinfo-unit to here. 
   @Input()
   user !: User;
-  check : boolean =false;
 
+  // Data binding from there to admins-usersinfo-unit. 
   @Output()
   second_step_Add_class : EventEmitter<Class> = new EventEmitter();
-
+  // Data binding from here to admins-usersinfo-unit. 
   @Output()
   second_check_include_classID2 : EventEmitter<Class> = new EventEmitter();
+
+  check : boolean =false;
+
 
   constructor() { 
   }
 
+
   ngOnInit(): void {
+    // Call the check_include_classID(function) when the page loading.  
     this.check_include_classID(this.user, this.class);
   }
 
+  // Add Class.
   first_step_Add_class(user_1 : User, class_1 : Class){
     if(user_1.ClassIDList.includes(class_1._id!)){
         alert("Already added");
@@ -40,6 +50,7 @@ export class AdminAvaliableClassListComponent implements OnInit {
   }
 
 
+  //Filter Classes which user doesn't enrolled.
   check_include_classID(user_1 : User, class_1 : Class){
     if(user_1.ClassIDList.includes(class_1._id!)){
       this.check = false;
